ee1fcad447c6fe394f2f4903783cd199f4d383aa
Fanis <fanismoutso16@gmail.com>
2025-12-11 17:45:26 +0200

    model switcher from small to big macbook with the following buttons

--- src/components/three/ModelSwitcher.jsx
+++ src/components/three/ModelSwitcher.jsx
@@ -1,7 +1,29 @@
 import {useRef} from "react";
+import {useGSAP} from "@gsap/react";
 import {PresentationControls} from "@react-three/drei";
+import gsap from 'gsap';
+
 import MacbookModel14 from "../models/Macbook-14.jsx";
 import MacbookModel16 from "../models/Macbook-16.jsx";
+
+const ANIMATION_PRESENTATION = 1;
+const OFFSET_DISTANCE = 5;
+
+const fadeMeshes = (group, opacity) => {
+    if(!group) return;
+
+    group.traverse((child) => {
+        if(child.isMesh) {
+            child.material.transparent = true;
+            gsap.to(child.material, {opacity, duration:ANIMATION_PRESENTATION})
+        }
+    })
+}
+
+const moveGroup = (group, x) => {
+    if(!group) return;
+    gsap.to(group.position, {x, duration:ANIMATION_PRESENTATION})
+}
 
 const ModelSwitcher = ({scale, isMobile}) => {
     const smallMacbookRef = useRef();
@@ -9,8 +31,30 @@ const ModelSwitcher = ({scale, isMobile}) => {
 
     const showLargeMacbook = scale === 0.08 || scale === 0.05;
 
+    useGSAP(() => {
+        if (showLargeMacbook) {
+            moveGroup(smallMacbookRef.current, -OFFSET_DISTANCE);
+            moveGroup(largeMacbookRef.current, 0);
+
+            fadeMeshes(smallMacbookRef.current, 0);
+            fadeMeshes(largeMacbookRef.current, 1);
+        } else {
+            moveGroup(smallMacbookRef.current, 0);
+            moveGroup(largeMacbookRef.current, -OFFSET_DISTANCE);
+
+            fadeMeshes(smallMacbookRef.current, 1);
+            fadeMeshes(largeMacbookRef.current, 0);
+
+        }
+
+    },[scale])
+
     const controlsConfig={
         snap: true,
+        speed: 1,
+        zoom: 1,
+        azimuth: [-Infinity, Infinity],
+        config: {mass:1, tension: 0, friction: 26}
     }
 
     return (
